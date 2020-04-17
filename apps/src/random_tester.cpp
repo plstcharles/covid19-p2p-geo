63acc7e733ee5e320807b2227b2bfcd5f3afd878
Pierre-Luc St-Charles <pierreluc.stcharles@gmail.com>
2020-04-17 12:08:36 -0400

    Add error handling to random tester app

--- apps/src/random_tester.cpp
+++ apps/src/random_tester.cpp
@@ -1,10 +1,16 @@
 
 /// dummy testing app that will run random region tree builds & queries via the API
 
+#include <iostream>
+
 #include "api.hpp"
 
-int main(int /*argc*/, char* argv[]) {
+int main(int argc, char* argv[]) {
     // assumes the first and only argument passed in is the data root directory path
+    if(argc < 2) {
+        std::cout << " ERROR: missing command line argument (data root path)" << std::endl;
+        return 1;
+    }
     testRandomBuildAndQueries(argv[1]);
     return 0;
 }

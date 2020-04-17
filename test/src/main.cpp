c21f4819ed29f68f2f05ce8f9abde3a5eb907ad6
Pierre-Luc St-Charles <pierreluc.stcharles@gmail.com>
2020-04-17 15:43:20 -0400

    Fixup main api tests & add mila coord check

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -3,15 +3,30 @@
 #include "gtest/gtest.h"
 #include "api.hpp"
 
-inline bool file_exists(const std::string& sFilePath) {
+inline bool checkPathExists(const std::string& sFilePath) {
     struct stat oBuffer;
     return (stat(sFilePath.c_str(), &oBuffer) == 0);
 }
 
 const std::string g_sDataRootPath = DATA_ROOT;
-const std::string g_sExpectedFullHDF5Path = g_sDataRootPath + "/full.hdf5";
 
 TEST(covid19_p2p_geo_api, data_exists) {
-    std::cout << "g_sExpectedFullHDF5Path = " << g_sExpectedFullHDF5Path << std::endl;
-    ASSERT_TRUE(file_exists(g_sExpectedFullHDF5Path));
+    const std::string g_sExpectedFullHDF5Path = g_sDataRootPath + "/full.hdf5";
+    ASSERT_TRUE(checkPathExists(g_sExpectedFullHDF5Path));
+    const std::string g_sExpectedCDHDF5Path = g_sDataRootPath + "/cd.hdf5";
+    ASSERT_TRUE(checkPathExists(g_sExpectedCDHDF5Path));
+    const std::string g_sExpectedDAHDF5Path = g_sDataRootPath + "/divisions";
+    ASSERT_TRUE(checkPathExists(g_sExpectedDAHDF5Path));
+}
+
+TEST(covid19_p2p_geo_api, random_build_and_queries) {
+    testRandomBuildAndQueries(g_sDataRootPath);
+}
+
+TEST(covid19_p2p_geo_api, mila_coord_check) {
+    // check if we can match the data obtained manually from censusmapper.ca
+    const std::string sExpectedMilaUID = "24661626";
+    const double dLat = 45.530637, dLon = -73.613989;
+    const std::string sUID = fetchRegionUID(dLat, dLon, g_sDataRootPath);
+    ASSERT_EQ(sUID, sExpectedMilaUID);
 }

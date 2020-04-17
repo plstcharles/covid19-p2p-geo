b14d5d577d976af0c97e3ddaacc452ce063f3515
Pierre-Luc St-Charles <pierreluc.stcharles@gmail.com>
2020-04-17 13:56:08 -0400

    Fixup curr test w/ data check

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,6 +1,17 @@
+#include <sys/stat.h>
+
 #include "gtest/gtest.h"
 #include "api.hpp"
 
-TEST(covid19_p2p_geo_api, dummy) {
-    ASSERT_TRUE(true);
+inline bool file_exists(const std::string& sFilePath) {
+    struct stat oBuffer;
+    return (stat(sFilePath.c_str(), &oBuffer) == 0);
+}
+
+const std::string g_sDataRootPath = DATA_ROOT;
+const std::string g_sExpectedFullHDF5Path = g_sDataRootPath + "/full.hdf5";
+
+TEST(covid19_p2p_geo_api, data_exists) {
+    std::cout << "g_sExpectedFullHDF5Path = " << g_sExpectedFullHDF5Path << std::endl;
+    ASSERT_TRUE(file_exists(g_sExpectedFullHDF5Path));
 }

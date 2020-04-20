6f52d37a384690e14387e769e35a803902904b8b
Pierre-Luc St-Charles <pierreluc.stcharles@gmail.com>
2020-04-20 19:30:18 -0400

    Add linux-only test for prep-from-scratch

--- test/src/test_api.cpp
+++ test/src/test_api.cpp
@@ -1,10 +1,20 @@
 #include <sys/stat.h>
 
+#ifdef __linux__
+#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <sys/stat.h>
+#include <sys/types.h>
+#endif //def(__linux__)
+
 #include "gtest/gtest.h"
 #include "private/generic_utils.hpp"
 #include "api.hpp"
 
 const std::string g_sDataRootPath = DATA_ROOT;
+
+const std::string g_sEmptyDataRootPath = "/tmp/covid19-p2p-geo-test";
 
 // check if we can match the data obtained manually from censusmapper.ca
 const uint32_t g_nExpectedMilaUID = 24661626;
@@ -31,6 +41,54 @@ TEST(covid19_p2p_geo_api, mila_prepared_coord_check) {
     const uint32_t nUID = fetchUID(g_dMilaLatitude, g_dMilaLongitude, g_sDataRootPath.c_str());
     ASSERT_EQ(nUID, g_nExpectedMilaUID);
 }
+
+#ifdef __linux__
+
+inline void copyFile(const std::string& sInput, const std::string& sOutput) {
+    std::ifstream hInput(sInput, std::ios::binary);
+    std::ofstream hOutput(sOutput, std::ios::binary);
+    hOutput << hInput.rdbuf();
+}
+
+TEST(covid19_p2p_geo_api, mila_full_coord_check_from_nothing) {
+    ASSERT_TRUE(checkPathExists("/tmp")); // can't run test if /tmp does not exist...
+    if(!checkPathExists(g_sEmptyDataRootPath)) {
+        ASSERT_EQ(mkdir(g_sEmptyDataRootPath.c_str(), 0777), 0);
+        ASSERT_EQ(mkdir((g_sEmptyDataRootPath + "/divisions").c_str(), 0777), 0);
+        copyFile(g_sDataRootPath + "/cd.hdf5", g_sEmptyDataRootPath + "/cd.hdf5");
+    }
+    // remove old divisions if the test was already executed
+    if(checkPathExists(g_sEmptyDataRootPath + "/divisions/2465.hdf5"))
+        remove((g_sEmptyDataRootPath + "/divisions/2465.hdf5").c_str());
+    if(checkPathExists(g_sEmptyDataRootPath + "/divisions/2466.hdf5"))
+        remove((g_sEmptyDataRootPath + "/divisions/2466.hdf5").c_str());
+    const char* acTargetFilenames =
+            prepareNear(g_dMilaLatitude, g_dMilaLongitude, g_sEmptyDataRootPath.c_str());
+    ASSERT_NE(acTargetFilenames, nullptr);
+    ASSERT_NE(acTargetFilenames[0], 0);
+    std::string sTargetFilenames = acTargetFilenames;
+    std::vector<std::string> vsTargetFilenames;
+    size_t nStrPos = 0u;
+    while((nStrPos = sTargetFilenames.find(',')) != std::string::npos) {
+        vsTargetFilenames.push_back(sTargetFilenames.substr(0, nStrPos));
+        sTargetFilenames.erase(0, nStrPos + 1u);
+    }
+    vsTargetFilenames.push_back(sTargetFilenames);
+    for(const auto& sTargetFilename: vsTargetFilenames) {
+        const std::string sInput = g_sDataRootPath + "/divisions/" + sTargetFilename + ".hdf5";
+        const std::string sOutput = g_sEmptyDataRootPath + "/divisions/" + sTargetFilename + ".hdf5";
+        copyFile(sInput, sOutput);
+    }
+    acTargetFilenames = prepareNear(g_dMilaLatitude, g_dMilaLongitude, g_sEmptyDataRootPath.c_str());
+    ASSERT_NE(acTargetFilenames, nullptr);
+    ASSERT_EQ(acTargetFilenames[0], 0);
+    ASSERT_GT(getCachedRegionTreeCount(), 0u);
+    const uint32_t nUID = fetchUID(g_dMilaLatitude, g_dMilaLongitude, g_sDataRootPath.c_str());
+    ASSERT_EQ(nUID, g_nExpectedMilaUID);
+}
+
+#endif //def(__linux__)
+
 
 TEST(covid19_p2p_geo_api, prep_and_release) {
     releaseUnusedCache(0);
